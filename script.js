f4970a855e58aa5b771367e99174b6dc97b073fc
swift1gh <princeyekunya523@gmail.com>
2024-05-15 15:27:07 +0000

    More blog content added (learn more)

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+
+
+
+function moreContent() {
+    var blogContents = document.querySelectorAll('.blog-content');
+
+    blogContents.forEach(function(section) {
+        var learnMoreButton = section.querySelector('.learn-more-btn');
+        var goBackButton = section.querySelector('.go-back');
+        var blogIntro = section.querySelector('.blog-intro');
+        var moreContent = section.querySelector('.more-content');
+
+        learnMoreButton.addEventListener('click', function () {
+            blogIntro.style.opacity = '0';
+            
+            setTimeout(function () {
+                blogIntro.style.display = 'none';
+                moreContent.style.height = '100%';
+                setTimeout(function () {
+                    moreContent.style.overflow = 'auto';
+                }, 500);
+                section.style.paddingTop = '7%';
+            }, 500);
+        });
+
+        goBackButton.addEventListener('click', function () {
+            blogIntro.style.opacity = '1';
+            moreContent.style.overflow = 'hidden';
+            moreContent.style.height = '0';
+            setTimeout(function () {
+                blogIntro.style.display = 'block';
+                section.style.paddingTop = '25%';
+            }, 500);
+        });
+    });
+}
+
+moreContent();
+
+
+
